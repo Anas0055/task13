34911bf157b0f0d1c6bd7b78cd978d0892f6c10f
Anas0055 <manaskhan969@gmail.com>
2024-01-23 02:37:27 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Guest List: If you could invite anyone, living or deceased,
+//  to dinner, who would you invite? Make a list that includes 
+//  at least three people you’d like to
+// invite to dinner. Then use your list to print a message 
+// to each person, inviting them to dinner.
+var array = ["ali", "shakir", "talha",];
+for (var z = 0; z < 3; z++) {
+    console.log("dear, you are invited  to dinner ".concat(array[z]));
+}
---
+++ index.ts
@@ -0,0 +1,12 @@
+// Guest List: If you could invite anyone, living or deceased,
+//  to dinner, who would you invite? Make a list that includes 
+//  at least three people you’d like to
+// invite to dinner. Then use your list to print a message 
+// to each person, inviting them to dinner.
+
+
+let array=["ali","shakir","talha",]
+for(let  z=0 ; z<3;z++)
+{
+    console.log(`dear, you are invited  to dinner ${array[z]}`)
+}
